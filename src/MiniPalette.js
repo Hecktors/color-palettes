edf3d4cb705defc774d8a2c8806e2700bd40ef5b
t <hectors.dev@gmail.com>
2020-05-08 14:44:21 -0400

    Added @material-ui/styles, palettes preview

---
+++ src/MiniPalette.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+  root: {
+    backgroundColor: '#fff',
+    padding: '0.5rem',
+    margin: '20px',
+    position: 'relative',
+    border: '1px solid #999',
+    borderRadius: '5px',
+    overflow: 'hidden',
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  colorBoxes: {
+    height: '150px',
+    width: '100%',
+    borderRadius: '3px',
+    overflow: 'hidden',
+  },
+  colorBox: {
+    display: 'inline-block',
+    width: '20%',
+    height: '25%',
+    margin: '0 auto',
+    position: 'relative',
+    marginBottom: '-3.5px',
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+  },
+  emoji: {},
+};
+
+function MiniPalette(props) {
+  const { classes, id, paletteName, emoji, colors } = props;
+  const colorBoxes = colors.map((color) => (
+    <div
+      key={color.name}
+      className={classes.colorBox}
+      style={{ backgroundColor: color.color }}
+    />
+  ));
+
+  return (
+    // <Link key={id} to={`/palette/${id}`}>
+    <div className={classes.root}>
+      <div className={classes.colorBoxes}>{colorBoxes}</div>
+      <h5 className={classes.title}>
+        {paletteName}
+        <span className={classes.emoji}>{emoji}</span>
+      </h5>
+    </div>
+    // </Link>
+  );
+}
+
+export default withStyles(styles)(MiniPalette);
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
+import MiniPalette from './MiniPalette';
 import './PaletteList.css';
-import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+  root: {
+    backgroundColor: 'blue',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  container: {
+    width: '50%',
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+  },
+  nav: {
+    display: 'flex',
+    width: '100',
+    justifyContent: 'space-between',
+    color: '#fff',
+  },
+  palettes: {
+    width: '100%',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 30%)',
+    gridGap: '5%',
+  },
+};
 
 class PaletteList extends Component {
   render() {
-    const links = this.props.palettes.map((palette) => (
-      <Link key={palette.id} to={`/palette/${palette.id}`}>
-        {palette.paletteName}
-      </Link>
+    const { classes } = this.props;
+    const miniPalettes = this.props.palettes.map((palette) => (
+      <MiniPalette key={palette.id} {...palette} />
     ));
 
     return (
-      <div className='PaletteList'>
-        <h1>Choose A Color Palette</h1>
-        {links}
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1>Choose A Color Palette</h1>
+          </nav>
+          <div className={classes.palettes}>{miniPalettes}</div>
+        </div>
       </div>
     );
   }
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
