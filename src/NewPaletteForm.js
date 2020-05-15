e0cb48c55429aa1e2ea43924e2ef1511c2661a3d
t <hectors.dev@gmail.com>
2020-05-15 10:27:22 -0400

    Added ChromePicker and Buttons to NewPaletteForm Drawer

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -10,8 +10,10 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import Typography from '@material-ui/core/Typography';
+import { ChromePicker } from 'react-color';
+import Button from '@material-ui/core/Button';
 
-const drawerWidth = 440;
+const drawerWidth = 400;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -72,7 +74,8 @@ const useStyles = makeStyles((theme) => ({
 
 function NewPaletteForm() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(true);
+  const [color, setColor] = React.useState('#88f');
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -81,7 +84,7 @@ function NewPaletteForm() {
   const handleDrawerClose = () => {
     setOpen(false);
   };
-
+  console.log('color', color);
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -121,6 +124,22 @@ function NewPaletteForm() {
           </IconButton>
         </div>
         <Divider />
+        <Typography variant='h4'>Design Your Palette</Typography>
+        <div>
+          <Button variant='contained' color='secondary'>
+            Clear Palette
+          </Button>
+          <Button variant='contained' color='primary'>
+            Random Color
+          </Button>
+        </div>
+        <ChromePicker
+          color={color}
+          onChangeComplete={(color) => setColor(color)}
+        />
+        <Button variant='contained' color='primary'>
+          Add Color
+        </Button>
       </Drawer>
       <main
         className={clsx(classes.content, {
